8aa76c900724d33999d7adec12d0a3c864ddbd59
Carlos Gamboa H <155551458+cgamboa-bayview@users.noreply.github.com>
2025-01-11 13:24:40 -0500

    init

---
+++ gl-ms-users/src/main/java/com/globallogic/test/gl_ms_users/GlMsUsersApplication.java
@@ -0,0 +1,13 @@
+package com.globallogic.test.gl_ms_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlMsUsersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlMsUsersApplication.class, args);
+	}
+
+}
